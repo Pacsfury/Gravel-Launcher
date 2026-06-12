902f417d5a5bb3a12b79c540d8baa3b15f056096
Pacsfury <280690649+Pacsfury@users.noreply.github.com>
2026-06-12 17:23:28 +0200

    Day 1 compiler: Tokenizer

---
+++ include/tokens.h
@@ -0,0 +1,38 @@
+#ifndef TOKENS_H
+#define TOKENS_H
+
+typedef enum {
+    TOKEN_EOF,
+    TOKEN_INT,
+    TOKEN_CHAR,
+    TOKEN_FLOAT,
+    TOKEN_ADD,
+    TOKEN_VAR_DEF,
+    TOKEN_VAR_INFER,
+    TOKEN_ASSIGN,
+    TOKEN_NAME,
+    TOKEN_TYPEIS_LIST
+} TokenType;
+
+
+typedef struct {
+    TokenType type;
+    char value[64];
+} Token;
+
+typedef enum {
+    NODE_NUMBER,
+    NODE_BINARY_OP
+} NodeType;
+
+typedef struct ASTNode {
+    NodeType type;
+    int int_value;
+    char op;
+    struct ASTNode* left;
+    struct ASTNode* right;
+} ASTNode;
+
+void tokenize(char file[]);
+
+#endif
