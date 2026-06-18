e56d251a832a4cb745dfdb669702a8a8aeb04d89
Pacsfury <280690649+Pacsfury@users.noreply.github.com>
2026-06-18 11:05:59 +0200

    Start LLVM translation, while some bugs detected

--- src/main.c
+++ src/main.c
@@ -3,30 +3,25 @@
 #include "../include/argc.h"
 #include "../include/tokens.h"
 #include "../include/ast.h"
+#include "../include/tollvm.h"
 
-// int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 
-//     ARGS_CONTEX ctx = { argc, argv };
-//     args_init(&ctx, argc, argv);
+    ARGS_CONTEX ctx = { argc, argv };
+    args_init(&ctx, argc, argv);
 
-//     if(hasArg(&ctx, "init")) {
-//         _launcherInit();
-//     }
+    if(hasArg(&ctx, "init")) {
+        _launcherInit();
+    }
 
-//     if(hasArg(&ctx, "end")) {
-//         _launcherFree();
-//     }
+    if(hasArg(&ctx, "end")) {
+        _launcherFree();
+    }
 
-//     _launcherFree();
-//     return 0;
-// }
-
-int main() {
-    // Simulamos los tokens de la instrucción: val x = 5 + 3 * 2
-    Token mock_tokens[] = {
-        { TOKEN_VAR_DEF, "val" },
+    Token mock_tokens[] = { // Example tokens
+        { TOKEN_INT, "int" },
         { TOKEN_NAME, "x" },
-        { TOKEN_ASSIGN, "=" },
+        { TOKEN_VAR_INFER, "=" },
         { TOKEN_INT, "5" },
         { TOKEN_ADD, "+" },
         { TOKEN_INT, "3" },
@@ -34,19 +29,39 @@ int main() {
         { TOKEN_INT, "2" },
         { TOKEN_EOF, "" }
     };
-    int token_count = 9;
 
-    // DEBUG: Yep, spanish
-    printf("--- Iniciando el Parser ---\n");
-    
-    // Ejecutamos tu función principal
-    ASTNode* ast_root = parse(mock_tokens, token_count);
+    to_llvm_ir(mock_tokens, 9);
 
-    printf("--- Árbol de Sintaxis Abstracta (AST) generado ---\n");
-    // Imprimimos el resultado empezando con profundidad 0
-    print_ast(ast_root, 0);
-
-    // TODO: Aquí deberías liberar la memoria con free() más adelante
+    _launcherFree();
     return 0;
 }
+
+// int main() { //DEBUG FUNCTION
+//     // Simulamos los tokens de la instrucción: val x := 5 + 3 * 2
+//     Token mock_tokens[] = { // Example tokens
+//         { TOKEN_INT, "int" },
+//         { TOKEN_NAME, "x" },
+//         { TOKEN_VAR_DEF, "=" },
+//         { TOKEN_INT, "5" },
+//         { TOKEN_ADD, "+" },
+//         { TOKEN_INT, "3" },
+//         { TOKEN_STAR, "*" },
+//         { TOKEN_INT, "2" },
+//         { TOKEN_EOF, "" }
+//     };
+//     int token_count = 9;
+
+//     // DEBUG: Yep, spanish
+//     printf("--- Iniciando el Parser ---\n");
+    
+//     // Ejecutamos tu función principal
+//     ASTNode* ast_root = parse(mock_tokens, token_count);
+
+//     printf("--- Árbol de Sintaxis Abstracta (AST) generado ---\n");
+//     // Imprimimos el resultado empezando con profundidad 0
+//     print_ast(ast_root, 0);
+
+//     // TODO: Aquí deberías liberar la memoria con free() más adelante
+//     return 0;
+// }
 
