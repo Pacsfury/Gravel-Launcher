10c3c604fc38c88ec320978bc834fc69fcbe79df
Pacsfury <280690649+Pacsfury@users.noreply.github.com>
2026-06-21 14:01:48 +0200

    Add single char out in LLVM (tried to)

--- src/tollvm.c
+++ src/tollvm.c
@@ -4,21 +4,28 @@
 #include "../include/ast.h"
 #include "../include/tollvm.h"
 
-void llvm_init_cout(FILE* outf) {
-    fprintf(outf,
-        "declare i32 @putchar(i32)\n\n"
-        "define void @cprint(i32 %charasc) {\n"
-        "entry:\n"
-        "    call i32 @putchar(i32 %charasc)\n"
-        "    ret void\n"
-        "}\n"
-    );
+void llvm_scho(FILE* outf, int sch, int* scho_init) {
+    if (*scho_init == 0) {
+        fprintf(outf,
+            "declare i32 @putchar(i32)\n\n"
+            "define void @cprint(i32 %%charasc) {\n"
+            "entry:\n"
+            "    %%0 = call i32 @putchar(i32 %%charasc)\n"
+            "    ret void\n"
+            "}\n\n"
+        );
+        *scho_init = 1;
+    }
+
+    fprintf(outf, "    call void @cprint(i32 %d)\n", sch);
 }
-void llvm_create_var(FILE* outf, const char* name[], const char* val[], const char* type[]) {
+
+void llvm_create_var(FILE* outf, const char* name, const char* val, const char* type) {
     fprintf(outf, "@%s = global %s %s, align 4\n", name, type, val);
 }
 
 int to_llvm_ir(const Token* tokens, int token_count) {
+    int scho_init = 0;
     FILE *outf;
 
     outf = fopen("output.ll", "w");
@@ -30,9 +37,16 @@ int to_llvm_ir(const Token* tokens, int token_count) {
     
     ASTNode* ast_root = parse(tokens, token_count);
 
-    fprintf(outf, "; ModuleID = 'output.ll'\n");
+    fprintf(outf, "; ModuleID = 'output.ll'\n\n");
     
     llvm_create_var(outf, "NAME", "32", "i32");
+    fprintf(outf, "\n");
+
+    fprintf(outf, "define i32 @main() {\nentry:\n");
+    
+    llvm_scho(outf, 65, &scho_init);
+
+    fprintf(outf, "    ret i32 0\n}\n");
 
     fclose(outf);
     return 0;
