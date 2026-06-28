f12709d788723664a7590a1b035c8c5790cd9dfa
Pacsfury <280690649+Pacsfury@users.noreply.github.com>
2026-06-28 12:27:43 +0200

    Add llvm transpilation and debug

--- include/tollvm.h
+++ include/tollvm.h
@@ -1,7 +1,8 @@
 #pragma once
+
 #include <stdio.h>
 #include "tokens.h"
+#include "ast.h"
 
-void llvm_scho(FILE* outf, int sch, int* scho_init);
-void llvm_create_var(FILE* outf, const char* name, const char* val, const char* type);
+
 int to_llvm_ir(const Token* tokens, int token_count);
--- src/tollvm.c
+++ src/tollvm.c
@@ -1,52 +1,137 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../include/tokens.h"
 #include "../include/ast.h"
 #include "../include/tollvm.h"
 
-void llvm_scho(FILE* outf, int sch, int* scho_init) {
-    if (*scho_init == 0) {
-        fprintf(outf,
-            "declare i32 @putchar(i32)\n\n"
-            "define void @cprint(i32 %%charasc) {\n"
-            "entry:\n"
-            "    %%0 = call i32 @putchar(i32 %%charasc)\n"
-            "    ret void\n"
-            "}\n\n"
-        );
-        *scho_init = 1;
+static char* safe_strdup(const char* s) {
+    char* d = malloc(strlen(s) + 1);
+    if (d == NULL) {
+        fprintf(stderr, "Memory allocation failed in LLVM generator\n");
+        exit(EXIT_FAILURE);
     }
-
-    fprintf(outf, "    call void @cprint(i32 %d)\n", sch);
+    strcpy(d, s);
+    return d;
 }
 
-void llvm_create_var(FILE* outf, const char* name, const char* val, const char* type) {
-    fprintf(outf, "@%s = global %s %s, align 4\n", name, type, val);
+
+void llvm_scho(FILE* outf, const char* val_to_print) {
+    fprintf(outf, "    call void @cprint(i32 %s)\n", val_to_print);
+}
+
+
+static char* compile_node(FILE* outf, ASTNode* node, int* register_count) {
+    if (!node) return NULL;
+
+    switch (node->type) {
+        case NODE_LITERAL:
+            return safe_strdup(node->data.literal.value);
+
+        case NODE_VARIABLE: {
+            int reg = (*register_count)++;
+            fprintf(outf, "    %%%d = load i32, ptr @%s, align 4\n", reg, node->data.literal.value);
+            
+            char buf[32];
+            snprintf(buf, sizeof(buf), "%%%d", reg);
+            return safe_strdup(buf);
+        }
+
+        case NODE_BINARY_OP: {
+            char* left = compile_node(outf, node->data.binary_op.left, register_count);
+            char* right = compile_node(outf, node->data.binary_op.right, register_count);
+            
+            const char* op_str = "";
+            switch(node->data.binary_op.op) {
+                case TOKEN_ADD:  op_str = "add"; break;
+                case TOKEN_SUB:  op_str = "sub"; break;
+                case TOKEN_STAR: op_str = "mul"; break;
+                case TOKEN_DIV:  op_str = "sdiv"; break;
+                default:         op_str = "add"; break;
+            }
+
+            int reg = (*register_count)++;
+            fprintf(outf, "    %%%d = %s i32 %s, %s\n", reg, op_str, left, right);
+            
+            free(left);
+            free(right);
+
+            char buf[32];
+            snprintf(buf, sizeof(buf), "%%%d", reg);
+            return safe_strdup(buf);
+        }
+
+        case NODE_DECLARATION: {
+            char* val = compile_node(outf, node->data.var_decl.value, register_count);
+            fprintf(outf, "    store i32 %s, ptr @%s, align 4\n", val, node->data.var_decl.name);
+            free(val);
+            return NULL; 
+        }
+
+
+        case NODE_SCHO: {
+            char* val = compile_node(outf, node->data.scho_stmt.value, register_count);
+            
+            llvm_scho(outf, val);
+            
+            free(val);
+            return NULL;
+        }
+        
+        case NODE_PROGRAM:
+            return NULL;
+    }
+    
+    return NULL;
 }
 
 int to_llvm_ir(const Token* tokens, int token_count) {
-    int scho_init = 0;
-    FILE *outf;
-
-    outf = fopen("output.ll", "w");
-
+    FILE *outf = fopen("output.ll", "w");
     if (outf == NULL) {
-        printf("Error: Could not open or create the file.\n");
+        printf("Error: Could not open or create output.ll file.\n");
         return 1; 
     }
     
     ASTNode* ast_root = parse(tokens, token_count);
 
-    fprintf(outf, "; ModuleID = 'output.ll'\n\n");
-    
-    llvm_create_var(outf, "NAME", "32", "i32");
+    if (!ast_root || ast_root->type != NODE_PROGRAM) {
+        fprintf(stderr, "Error: Invalid AST Root.\n");
+        fclose(outf);
+        return 1;
+    }
+
+    fprintf(outf, "; ModuleID = 'output.ll'\n");
+    fprintf(outf, "target datalayout = \"e-m:e-i64:64-f80:128-n8:16:32:64-S128\"\n\n");
+
+    fprintf(outf, "declare i32 @putchar(i32)\n\n");
+    fprintf(outf, "define void @cprint(i32 %%charasc) {\n");
+    fprintf(outf, "entry:\n");
+    fprintf(outf, "    %%0 = call i32 @putchar(i32 %%charasc)\n");
+    fprintf(outf, "    ret void\n");
+    fprintf(outf, "}\n\n");
+
+    for (int i = 0; i < ast_root->data.program.count; i++) {
+        ASTNode* stmt = ast_root->data.program.statements[i];
+        if (stmt->type == NODE_DECLARATION) {
+            fprintf(outf, "@%s = global i32 0, align 4\n", stmt->data.var_decl.name);
+        }
+    }
+
     fprintf(outf, "\n");
 
-    fprintf(outf, "define i32 @main() {\nentry:\n");
+    fprintf(outf, "define i32 @main() {\n");
+    fprintf(outf, "entry:\n");
     
-    llvm_scho(outf, 65, &scho_init);
+    int register_count = 1;
 
-    fprintf(outf, "    ret i32 0\n}\n");
+    for (int i = 0; i < ast_root->data.program.count; i++) {
+        ASTNode* stmt = ast_root->data.program.statements[i];
+        char* leftover = compile_node(outf, stmt, &register_count);
+        if (leftover) free(leftover); 
+    }
+
+    fprintf(outf, "    ret i32 0\n");
+    fprintf(outf, "}\n");
 
     fclose(outf);
     return 0;
