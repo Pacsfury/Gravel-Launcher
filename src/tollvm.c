166930c6ed841c1c640587c23b682e32757f41b3
Pacsfury <280690649+Pacsfury@users.noreply.github.com>
2026-08-02 21:15:48 +0200

    Better .ll compilation

--- src/tollvm.c
+++ src/tollvm.c
@@ -497,6 +497,11 @@ int to_llvm_ir(const Token* tokens, int token_count, ARGS_CONTEX* ctx) {
     printf("Compiled Successfully\n");
 
     if (!hasArg(ctx, "-wE")) {
+        #ifdef _WIN32
+            system(".\\llvm\\llvm.exe");
+        #else
+            system("./llvm/llvm.exe");
+        #endif
         exit(0);
     }
     return 0;
