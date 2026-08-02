bad7cef1a9d7ce7e96ba1e1641e6499f9b22c200
Pacsfury <280690649+Pacsfury@users.noreply.github.com>
2026-08-02 16:20:42 +0200

    Add PR template

--- tests/tests.py
+++ tests/tests.py
@@ -2,7 +2,7 @@ import subprocess
 from pathlib import Path
 
 def execute(file):
-    subprocess.run(['./gravel', 'run', "./tests/" + file])
+    subprocess.run(['./gravel', 'run', "./tests/" + file], check=True)
 
     if Path("./tests/" + file.replace(".grv", ".txt")).read_text(encoding="utf-8").strip() != Path("output.ll").read_text(encoding="utf-8").strip():
         print("file " + file + " is not passing the tests\n")
