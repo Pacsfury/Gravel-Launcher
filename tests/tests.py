1df659f084cb61c412bddd4c74a9073b96db18ef
Pacsfury <280690649+Pacsfury@users.noreply.github.com>
2026-08-22 11:25:55 +0200

    .

--- tests/tests.py
+++ tests/tests.py
@@ -26,7 +26,7 @@ def execute(file):
     if expected_path.read_text(encoding="utf-8").strip() != output_path.read_text(encoding="utf-8").strip():
         print(f"File {file} failed verification.")
 
-        print(f"\n\n{expected_path.read_text(encoding="utf-8").strip()}\n\n{output_path.read_text(encoding="utf-8").strip()}\n\n")
+        print(f'\n\n{expected_path.read_text(encoding="utf-8").strip()}\n\n{output_path.read_text(encoding="utf-8").strip()}\n\n')
         return 1
     
     print(f"{file} passed successfully.")
